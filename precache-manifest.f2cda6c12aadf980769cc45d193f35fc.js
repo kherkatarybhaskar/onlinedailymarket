bb152743a6f57107288fc3746d69395edc475a4c
Bhaskar Jyoti Kherkatary <kherkatarybhaskar@gmail.com>
2021-03-14 11:15:08 +0530

    Updates

---
+++ precache-manifest.f2cda6c12aadf980769cc45d193f35fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c506e7e04dfc80efcedda2d3b6025bf4",
+    "url": "/onlinedailymarket/index.html"
+  },
+  {
+    "revision": "de05103f94625440fa62",
+    "url": "/onlinedailymarket/static/css/3.56d8f420.chunk.css"
+  },
+  {
+    "revision": "8c3dda6ffa4f8e4a3faf",
+    "url": "/onlinedailymarket/static/css/main.ed6d0169.chunk.css"
+  },
+  {
+    "revision": "e50227b9897b2ce41338",
+    "url": "/onlinedailymarket/static/js/2.e55735f0.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/onlinedailymarket/static/js/2.e55735f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de05103f94625440fa62",
+    "url": "/onlinedailymarket/static/js/3.1b853be2.chunk.js"
+  },
+  {
+    "revision": "8c3dda6ffa4f8e4a3faf",
+    "url": "/onlinedailymarket/static/js/main.7599d2d6.chunk.js"
+  },
+  {
+    "revision": "a8f438e78618227122ff",
+    "url": "/onlinedailymarket/static/js/runtime-main.b75cb246.js"
+  },
+  {
+    "revision": "cc9495e9e9e51452e26e413901a7d72b",
+    "url": "/onlinedailymarket/static/media/attaimage.cc9495e9.png"
+  },
+  {
+    "revision": "668b8e05db3188ae782bed865dc04e93",
+    "url": "/onlinedailymarket/static/media/logo.668b8e05.jpg"
+  }
+]);
